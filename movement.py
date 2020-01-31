c1ec0a6f7ebc55740b914213a061899c81408f40
Benjamin Manson <benjmanson87@gmail.com>
2020-01-31 15:04:07 +0000

    Add scan skeleton

--- movement.py
+++ movement.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python
-# import rospy
-# from geometry_msgs.msg import Twist
-# from sensor_msgs.msg import LaserScan
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
-# def laser_scan_callback(data):
-# 	print data.ranges
+def laser_scan_callback(data):
+	print data.ranges
 
-# def read_laser_scan_data():
-# 	rospy.Subscriber('scan', LaserScan, laser_scan_callback)
+def read_laser_scan_data():
+	rospy.Subscriber('scan', LaserScan, laser_scan_callback)
 
 def move(speed=1):
-	# pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
-	# mc = Twist()
-	# mc.linear.x = speed
-	# mc.angular.z = 0
-	# pub.publish(mc)
-	if (speed == 1):
-		print("Moving forward")
-	else:
-		print("Moving backwards")
-
-def stop():
-	# pub = rospy.Publisher('cmd_stop', Twist, queue_size = 10)
-	# mc = Twist()
-	# mc.linear.x = 0
-	# mc.angular.z = 0
-	# pub.publish(mc)
-	print("Stopped")
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+	mc = Twist()
+	mc.linear.x = speed
+	mc.angular.z = 0
+	pub.publish(mc)
+	# if (speed > 0):
+	# 	print("Moving forward")
+	# else:
+	# 	print("Moving backwards")
 
 def turn(speed=0.75):
-	# pub = rospy.Publisher('cmd_trn', Twist, queue_size = 10)
-	# mc = Twist()
-	# mc.linear.x = 0
-	# mc.angular.z = speed
-	# pub.publish(mc)
-	if speed == 0.75:
-		print("Turning right")
-	else:
-		print("Turning left")
+	pub = rospy.Publisher('cmd_trn', Twist, queue_size = 10)
+	mc = Twist()
+	mc.linear.x = 0
+	mc.angular.z = speed
+	pub.publish(mc)
+	# if speed == 0.75:
+	# 	print("Turning right")
+	# else:
+	# 	print("Turning left")
+
+def stop():
+	pub = rospy.Publisher('cmd_stop', Twist, queue_size = 10)
+	mc = Twist()
+	mc.linear.x = 0
+	mc.angular.z = 0
+	pub.publish(mc)
+	# print("Stopped")
+
+def scan(ranges):
+	if len(ranges) == 720:
+		print("True")
