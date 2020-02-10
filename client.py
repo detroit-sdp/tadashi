31e1cca313600f01e794ef75bd25f4cb5357b050
Benjamin Manson <benjmanson87@gmail.com>
2020-02-10 16:51:47 +0000

    Basic client implementation

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+msgFromClient = raw_input("Input something to send to server: ")
+print(msgFromClient)
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("localhost", 20001)
+bufferSize = 1024
+
+# Create a UDP socket at client side
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+msg = "Message from Server {}".format(msgFromServer[0])
+
+print(msg)
