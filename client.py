fb6451d5c421453eed92c6bdcfef96e65f6a5120
Benjamin Manson <benjmanson87@gmail.com>
2020-02-21 23:26:38 +0000

    Comment out more receive

--- client.py
+++ client.py
@@ -13,8 +13,6 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Send to server using created UDP socket
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-#msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
+# msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+# msg = "Message from Server {}".format(msgFromServer[0])
+# print(msg)
