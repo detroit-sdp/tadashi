c3930e500bf1c117477a10f2db915311df38192c
Benjamin Manson <benjmanson87@gmail.com>
2020-02-23 13:34:20 +0000

    Send to Turtlebot

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 
-msgFromClient = raw_input("Input something to send to server: ")
-print(msgFromClient)
-bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("localhost", 20001)
+serverAddressPort = ('192.168.105.172', 20001)
 bufferSize = 1024
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-# msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-# msg = "Message from Server {}".format(msgFromServer[0])
-# print(msg)
+while(True):
+    msgFromClient = raw_input("Input something to send to server: ")
+    print(msgFromClient)
+    
+    bytesToSend = str.encode(msgFromClient)
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+    # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    # msg = "Message from Server {}".format(msgFromServer[0])
+    # print(msg)
