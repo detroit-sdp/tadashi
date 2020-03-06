480872e93d5250dae881b4adbd53e7e81069c24c
Benjamin Manson <benjmanson87@gmail.com>
2020-03-06 22:32:55 +0000

    Change point to single item of tuple in ROOMS

--- tadashi/scripts/const.py
+++ tadashi/scripts/const.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 ROOMS = {
-	1: (x,y, 'F'),
-	2: (x,y, 'F'),
-	3: (x,y, 'F')
-	'BASE': (0.0, 0.0, 'F')
+	1: ((x,y), 'F'),
+	2: ((x,y), 'F'),
+	3: ((x,y), 'F')
+	'BASE': ((0.0, 0.0), 'F')
 }
 
 COMMANDS = {'GOTO', 'LIFT'}
