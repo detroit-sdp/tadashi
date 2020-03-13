d822c103f0a7d54b59c7ba19db3458e14b14c8a3
ben <benjmanson87@gmail.com>
2020-03-13 12:24:18 +0000

    Bug fixes and return packet setup

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
 
+localIP = ''
+localPort = 20002
 serverAddressPort = ('localhost', 20001)
 bufferSize = 1024
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPClientSocket.bind((localIP, localPort))
+UDPClientSocket.settimeout(None)
 
 while(True):
     msgFromClient = raw_input("Input something to send to server: ")
@@ -12,6 +16,15 @@ while(True):
     bytesToSend = str.encode(msgFromClient)
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-    # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    # msg = "Message from Server {}".format(msgFromServer[0])
-    # print(msg)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    msg = "Message from Server {}".format(msgFromServer[0])
+    print(msg)
+
+    if msgFromServer[0] == 'BASE':
+        continue
+    else:
+        save = msgFromServer[0]
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        if msgFromServer[0] != save:
+            msg = "Message from Server {}".format(msgFromServer[0])
+            print(msg)
