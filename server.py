7e4984de0c68662a6cec042aa00bc406f131ee03
Benjamin Manson <benjmanson87@gmail.com>
2020-03-13 14:53:57 +0000

    Forward packets to correct location

--- server.py
+++ server.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python
 import socket
-localIP = ''    # I don't think you change this, but if it fails then set to 'localhost', '127.0.0.1',
-				# or '192.168.105.x' as shown in ifconfig
+from const import *
 
+localIP = ''
 localPort = 20001
 bufferSize = 1024
-
-# msgFromServer = 'test return'
-# bytesToSend = str.encode(msgFromServer)
-# YUCHEN listening on port 20002
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 UDPServerSocket.settimeout(None)
 
-print('UDP server up and listening')
-
 while(True):
 	bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-	message = bytesAddressPair[0]
+	command = bytesAddressPair[0]
 	address = bytesAddressPair[1]
+
+	command = commands[0]
+	args = commands[1:]
+	sendaddress = None
+
+	if command.upper() == 'STATUS':
+		sendaddress = ('192.168.105.111', 20002)
+	elif command.upper() == 'GOTO':
+		sendaddress = ('192.168.105.95', 20001)
 
 	print('Message from Client:{}'.format(message))
 	print('Client IP Address:{}'.format(address))
 
-	sendaddress = ('192.168.105.95', 20001)
-	# yuchenaddress = (address[0], 20002)
 	UDPServerSocket.sendto(str.encode(message), sendaddress)
